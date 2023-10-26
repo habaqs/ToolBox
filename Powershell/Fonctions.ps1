3f21687bea1c667de86d4e87f3689cac9ab600bb
habaqs <habacuc.reginald@gmail.com>
2023-10-26 20:27:53 +0200

    Update clean teams + fichier fonctions

---
+++ Powershell/Fonctions.ps1
@@ -0,0 +1,32 @@
+# Dans ce fichier un regroupement de fonctions utile pour l'edition de script
+
+
+
+function checkPowershellAdmin {
+    <#
+       .Description
+       Retourne Vrais si la session powershell a été démarré en tant qu'administrateur.
+    #>
+    return ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
+
+function CleanFolders {
+    <#
+        .DESCRIPTION
+        !!! La fonction ne demande aucune validation lors de la suppresion des fichiers/dossiers
+        Supprimer le contenu du dossier (fichiers et dossiers) sans supprimer le dossier lui-même.
+        En cas d'erreur le fichier est ignoré
+        !!! La fonction ne demande aucune validation lors de la suppresion des fichiers/dossiers
+
+        .PARAMETER $folder
+        liste de dossier a nettoyer 
+    #>
+    param (
+        $folders
+    )
+
+    foreach($folder in $folders){ 
+        Get-ChildItem -Path $folder | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
+    }
+}
---
+++ Powershell/cleanteams.ps1
@@ -0,0 +1,25 @@
+# Script pour vider le cache de Microsoft Teams. vous devez arreter teams avant de l'executer 
+
+
+$foldersTocleean=@(
+    "$env:APPDATA\Microsoft\Teams\blob_storage\",
+    "$env:APPDATA\Microsoft\Teams\Cache\",
+    "$env:APPDATA\Microsoft\Teams\databases\",
+    "$env:APPDATA\Microsoft\Teams\GPUCache\",
+    "$env:APPDATA\Microsoft\Teams\IndexedDB\",
+    "$env:APPDATA\Microsoft\Teams\Local Storage\",
+    "$env:APPDATA\Microsoft\Teams\tmp\"
+)
+
+# voir fichier Fonctions
+function CleanFolders {
+    param (
+        $folders
+    )
+
+    foreach($folder in $folders){ 
+        Get-ChildItem -Path $folder | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
+    }
+}
+
+CleanFolders($foldersTocleean)
