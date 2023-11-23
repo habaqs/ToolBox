7bafbcf885edb22913c931c8c7a12aa554052443
habaqs <habacuc.reginald@gmail.com>
2023-11-23 07:33:45 +0100

    Ajout du script MappageLecteur

---
+++ Scripts Powershell/MappageLecteur.ps1
@@ -0,0 +1,18 @@
+# Dans ce fichier un script permettant de monter un lecteur mappé.
+# By @habaqs
+
+
+### Variables
+$LettreDuLecteur = "L:"
+$CheminReseau = "\\Serveur\NomDuPartage"
+$NomUtilisateur = "MonDomaine\User01"
+$MotDePasse = "UnFauxMotDePasse"
+# Pour que le lecteur soit présent au prochain démarrage
+$ConnexionAutomatique = $true
+
+### Supprimer le lecteur mappé
+Remove-PSDrive -Name $LettreDuLecteur -Force
+
+### Créer un lecteur mappé
+$Net = New-Object -ComObject WScript.Network
+$Net.MapNetworkDrive($LettreDuLecteur, $CheminReseau, $ConnexionAutomatique, $NomUtilisateur, $MotDePasse)
